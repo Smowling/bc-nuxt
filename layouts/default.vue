ff98a57afd2ef5459c543e1f76425400baf6b06f
Daniel Smółka <smowling@gmail.com>
2024-01-30 14:18:31 +0100

    tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,15 @@
 <template>
-    <header>
-        <nav>
-            <ul>
+    <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+            <NuxtLink to="/" class="font-bold">Bikecheck!</NuxtLink>
+            <ul class="flex gap-4">
                 <li><NuxtLink to="/">HOME</NuxtLink></li>
                 <li><NuxtLink to="/about">ABOUT</NuxtLink></li>
                 <li><NuxtLink to="/bikeshop/123">123</NuxtLink></li>
             </ul>
         </nav>
     </header>
-    <div>
+    <div class="container mx-auto p-4">
         <slot />
     </div>
 </template>
