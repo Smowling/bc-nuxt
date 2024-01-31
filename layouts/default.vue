7d7d4a9f91b0b3c7597e3b7426db19e8fde038bb
Daniel Smółka <smowling@gmail.com>
2024-01-31 22:13:08 +0100

    darkmode

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,13 @@
                 <li><NuxtLink to="/">HOME</NuxtLink></li>
                 <li><NuxtLink to="/about">ABOUT</NuxtLink></li>
                 <li><NuxtLink to="/bikeshop/123" class="btn">123</NuxtLink></li>
+                <UButton
+                    :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+                    color="gray"
+                    variant="ghost"
+                    aria-label="Theme"
+                    @click="isDark = !isDark"
+                    />
             </ul>
         </nav>
     </header>
@@ -19,3 +26,16 @@ body {
     background-color: #fefefe;
 }
 </style>
+
+<script setup>
+const colorMode = useColorMode()
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+
+</script>
