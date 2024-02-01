1e3480624d30b568ebf21666602faecd3a0d381f
Daniel Smółka <smowling@gmail.com>
2024-02-01 12:33:54 +0100

    login/logout

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,8 @@
             <ul class="flex gap-4">
                 <li><NuxtLink to="/">HOME</NuxtLink></li>
                 <li><NuxtLink to="/about">ABOUT</NuxtLink></li>
-                <li><NuxtLink to="/bikeshop/123" class="btn">123</NuxtLink></li>
+                <li v-show="user"><UButton @click="signOut">Logout</UButton></li>
+                <li v-show="!user"><UButton to="/login">Login</UButton></li>
                 <UButton
                     :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
                     color="gray"
@@ -28,6 +29,7 @@ body {
 </style>
 
 <script setup>
+// dark-light mode
 const colorMode = useColorMode()
 const isDark = computed({
   get () {
@@ -37,5 +39,19 @@ const isDark = computed({
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
 })
+
+// user?
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+async function signOut() {
+  try {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+    user.value = null
+  } catch (error) {
+    alert(error.message)
+  }
+}
 
 </script>
