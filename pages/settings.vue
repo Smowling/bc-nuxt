96ac218cfe4412ad315305c28139997c3993f446
Daniel Smółka <smowling@gmail.com>
2024-02-07 18:48:12 +0100

    settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
-        <p>Settings page!</p>
+        <p>Bikes</p>
+        <ul>
+            <li v-for="(bike, index) in settings" :key="index">
+                <p> {{ bike.brand }}</p>
+                <p> {{ bike.model }}</p>
+                <p> {{ bike.year }}</p>
+            </li>
+        </ul>
     </div>
 </template>
+
+
+<script setup>
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+const settings = ref('')
+
+
+onMounted(() => {
+    getUserSettings();
+})
+
+async function getUserSettings() {
+  const { data, error } = await supabase.from('bikes').select().eq("user_id", user.value.id)
+  if (error) { console.log(error.message) }
+  else {
+    return data
+  }
+}
+
+</script>
