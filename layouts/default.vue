018e937b9e773a1e913ca531cfc4fdbaf4e05d07
Daniel Smółka <smowling@gmail.com>
2024-02-08 14:20:10 +0100

    add bike

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,32 @@
 <template>
-    <header class="shadow-sm">
-      <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
-      <nav class="container mx-auto p-4 flex justify-between">
-        <ULink to="/" class="font-bold">Bikecheck!</ULink>
-            <ul class="flex gap-4">
-                <li><ULink to="/">HOME</ULink></li>
-                <li><ULink to="/about">ABOUT</ULink></li>
-                <li v-show="user"><UButton @click="signOut">Logout</UButton></li>
-                <li v-show="!user"><UButton to="/login">Login</UButton></li>
-                <UButton
-                    :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-                    color="gray"
-                    variant="ghost"
-                    aria-label="Theme"
-                    @click="isDark = !isDark"
-                    />
-              </ul>
-        </nav>
-    </header>
-    <div class="container mx-auto p-4">
-        <slot />
-    </div>
-    <footer>
-      <p>All rights reserved.</p>
-    </footer>
+  <header class="shadow-sm">
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+    <nav class="container mx-auto p-4 flex justify-between">
+      <ULink to="/" class="font-bold">Bikecheck!</ULink>
+      <ul class="flex gap-4">
+        <li>
+          <ULink to="/">HOME</ULink>
+        </li>
+        <li>
+          <ULink to="/about">ABOUT</ULink>
+        </li>
+        <li v-show="user">
+          <UButton @click="signOut">Logout</UButton>
+        </li>
+        <li v-show="!user">
+          <UButton to="/login">Login</UButton>
+        </li>
+        <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray" variant="ghost"
+          aria-label="Theme" @click="isDark = !isDark" />
+      </ul>
+    </nav>
+  </header>
+  <div class="container mx-auto p-4">
+    <slot />
+
+    <Footer />
+
+  </div>
 </template>
 
 
@@ -32,27 +35,27 @@
 // horizontal nav
 const links = [
   {
-  label: 'Home',
-  to: '/'
-},
-{
-  label: 'About',
-  to: '/about'
-},
-{
-  label: 'Settings',
-  to: '/settings'
-}
+    label: 'Home',
+    to: '/'
+  },
+  {
+    label: 'About',
+    to: '/about'
+  },
+  {
+    label: 'Settings',
+    to: '/settings'
+  }
 
 ]
 
 // dark-light mode
 const colorMode = useColorMode()
 const isDark = computed({
-  get () {
+  get() {
     return colorMode.value === 'dark'
   },
-  set () {
+  set() {
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
 })
--- pages/settings.vue
+++ pages/settings.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <p>Bikes</p>
+        <UButton :label="add_bike ? 'Cancel' : 'Add'" @click="add_bike = !add_bike"
+            :icon="add_bike ? '' : 'i-heroicons-plus-solid'" />
+        <UContainer v-if="add_bike">
+            <UInput v-model="bikeForm['brand']" placeholder="Brand" />
+            <UInput v-model="bikeForm['model']" placeholder="Model" />
+            <UInput v-model="bikeForm['year']" placeholder="Year" />
+            <UButton label="Add" @click="addBike()" />
+        </UContainer>
+
         <ul>
             <li v-for="(bike, index) in settings" :key="index">
                 <UCard>
@@ -21,22 +30,43 @@
 const user = useSupabaseUser()
 const supabase = useSupabaseClient()
 const settings = ref('')
+const add_bike = ref(false)
 
+const bikeForm = ref({
+    brand: "",
+    model: "",
+    year: "",
+    user_id: user.value.id,
+})
 
 onMounted(() => {
     getUserSettings();
 })
 
-async function deleteBike(bike_id){
+async function deleteBike(bike_id) {
     const { error } = await supabase.from("bikes").delete().eq("id", bike_id)
+    if (error) {
+        console.log(error.message)
+    }
+    else {
+        getUserSettings()
+    }
+}
+
+async function addBike() {
+    console.log(bikeForm.value)
+    const { error } = await supabase.from("bikes").insert(bikeForm.value)
+    if (error) {
+        console.log(error.message)
+    }
 }
 
 async function getUserSettings() {
-  const { data, error } = await supabase.from('bikes').select().eq("user_id", user.value.id)
-  if (error) { console.log(error.message) }
-  else {
-    settings.value = data
-  }
+    const { data, error } = await supabase.from('bikes').select().eq("user_id", user.value.id)
+    if (error) { console.log(error.message) }
+    else {
+        settings.value = data
+    }
 }
 
 </script>
