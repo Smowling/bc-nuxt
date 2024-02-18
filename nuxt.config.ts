658fec4459a85c7e131bf81b1ea45877548238a1
Daniel Smółka <smowling@gmail.com>
2024-02-18 12:48:59 +0100

    Added '@nuxtjs/tailwindcss', 'shadcn-nuxt' modules and configured 'shadcn' in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  "modules": ['@nuxtjs/supabase', '@nuxt/ui'],
+  "modules": ['@nuxtjs/supabase', '@nuxt/ui', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   "devtools": { enabled: true },
   "supabase": { redirect: false },
   "colorMode": {
     "preference": 'light'
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
   },
   components: [
     {
