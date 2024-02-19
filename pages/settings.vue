576eeb2d22170154b634ce9ebb4efefc2613b723
Daniel Smółka <smowling@gmail.com>
2024-02-19 17:13:10 +0100

    Added Popover component and integrated it into settings page

--- pages/settings.vue
+++ pages/settings.vue
@@ -6,7 +6,7 @@
         <UContainer v-if="add_bike">
             <UInput v-model="bikeForm['brand']" placeholder="Brand" />
             <UInput v-model="bikeForm['model']" placeholder="Model" />
-            <!-- <Popover>
+            <Popover>
                 <PopoverTrigger as-child>
                     <Button :variant="'outline'" :class="cn(
                         'w-[280px] justify-start text-left font-normal',
@@ -19,7 +19,7 @@
                 <PopoverContent class="w-auto p-0">
                     <Calendar v-model="date" />
                 </PopoverContent>
-            </Popover> -->
+            </Popover>
             <UButton label="Add" @click="addBike()" />
         </UContainer>
 
@@ -30,6 +30,44 @@
                 <UButton label="Delete" icon="i-heroicons-minus-solid" @click="deleteBike(bike.id)" />
             </li>
         </ul>
+
+        <Popover>
+            <PopoverTrigger as-child>
+                <Button variant="outline">
+                    Open popover
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent class="w-80">
+                <div class="grid gap-4">
+                    <div class="space-y-2">
+                        <h4 class="font-medium leading-none">
+                            Dimensions
+                        </h4>
+                        <p class="text-sm text-muted-foreground">
+                            Set the dimensions for the layer.
+                        </p>
+                    </div>
+                    <div class="grid gap-2">
+                        <div class="grid grid-cols-3 items-center gap-4">
+                            <Label for="width">Width</Label>
+                            <Input id="width" type="text" default-value="100%" class="col-span-2 h-8" />
+                        </div>
+                        <div class="grid grid-cols-3 items-center gap-4">
+                            <Label for="maxWidth">Max. width</Label>
+                            <Input id="maxWidth" type="text" default-value="300px" class="col-span-2 h-8" />
+                        </div>
+                        <div class="grid grid-cols-3 items-center gap-4">
+                            <Label for="height">Height</Label>
+                            <Input id="height" type="text" default-value="25px" class="col-span-2 h-8" />
+                        </div>
+                        <div class="grid grid-cols-3 items-center gap-4">
+                            <Label for="maxHeight">Max. height</Label>
+                            <Input id="maxHeight" type="text" default-value="none" class="col-span-2 h-8" />
+                        </div>
+                    </div>
+                </div>
+            </PopoverContent>
+        </Popover>
     </div>
 </template>
 
