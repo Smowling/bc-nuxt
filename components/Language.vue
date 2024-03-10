c62ac2026756d44e8db4a1327bc932ed79c5ff22
Daniel Smółka <smowling@gmail.com>
2024-03-10 22:30:12 +0100

    lang switch

---
+++ components/Language.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <select v-model="language" class="border-solid">
+            <option 
+            v-for="locale in locales" 
+            :value="locale.code"
+            :key="locale.code"
+            >{{ locale.flag }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { locales, locale, setLocale } = useI18n();
+const language = computed({
+    get: () => locale.value,
+    set: (value: string) => setLocale(value)
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
