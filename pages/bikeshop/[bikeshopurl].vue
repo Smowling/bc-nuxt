2840251bd17e59936644a76c4611f04a0a45cbe1
Daniel Smółka <smowling@gmail.com>
2024-03-21 18:12:17 +0100

    services

--- pages/bikeshop/[bikeshopurl].vue
+++ pages/bikeshop/[bikeshopurl].vue
@@ -3,21 +3,11 @@
     <template #default>
       <div>
         <UContainer>
-          <p>{{ bikeshop.name }}</p>
-          <!-- <p>{{ bikeshop[0].bikeshop }}</p>
-        <p>{{ bikeshop[0].description }}</p>
-        <p>{{ bikeshop[0].email }}</p> -->
+          <p>{{ bikeshop.value[0].bikeshop }}</p>
+          <p>{{ bikeshop.value[0].description }}</p>
         </UContainer>
-        <!-- <UCard v-for="(bs, index) in bikeshop" :key="index">
-          <template #header>
-            <h2>{{ bs.service }}</h2>
-            <p>{{ bs.details }}</p>
-          </template>
-          <template #footer>
-            <p>{{ bs.price }}</p>
-          </template>
-        </UCard> -->
-        <!-- <ServiceCard/> -->
+
+        <ServiceCard :bikeshop="bikeshop.value"></ServiceCard>
       </div>
     </template>
 
@@ -34,24 +24,22 @@
 const supabase = useSupabaseClient()
 const bikeshop = useBikeshopStore();
 
-// async function getBikeshop() {
-//   const { data, error } = await supabase.from('bikeshop_and_services').select().eq("url", bikeshopurl)
-//   if (error) { console.log(error.message) }
-//   else {
-//     bikeshop.value = data
-//   }
-// }
+const route = useRoute()
+const bikeshopurl = route.params.bikeshopurl
+
+async function getBikeshop() {
+  const { data, error } = await supabase.from('bikeshop_and_services').select().eq("url", bikeshopurl)
+  if (error) { console.log(error.message) }
+  else {
+    bikeshop.value = data
+  }
+}
 
 
 onMounted(() => {
-  // getBikeshop();
-  async () => { await store.dispatch('selectBikeshop', bikeshop)}
-  console.log(bikeshop)
+  console.log(bikeshopurl)
+  getBikeshop();
+  console.log(bikeshop.value);
 })
 
-// onUpdated(() => {
-//   getBikeshop();
-// })
-
 </script>
-
