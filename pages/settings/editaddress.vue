8b969b7e210d48c7470d2e6093ae039f27f1b248
Daniel Smółka <smowling@gmail.com>
2024-03-26 19:52:15 +0100

    edit address prep

---
+++ pages/settings/editaddress.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Card>
+            <CardHeader>
+            </CardHeader>
+            <CardContent>
+                {{ address.city }}
+            </CardContent>
+            <CardContent>
+                {{ address.street }}
+            </CardContent>
+            <CardContent>
+                {{ address.number }}
+                {{ index }}
+            </CardContent>
+            <CardFooter>
+                <Button><span>{{ $t('btnEdit') }}</span></Button>
+                <Button @click="addressDelete(address.id, index)"><span>{{ $t('btnDelete') }}</span></Button>
+            </CardFooter>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+
+defineProps(["address", "index"])
+const emit = defineEmits(["addressDelete"])
+
+function addressDelete(addressid, index) {
+    emit("addressDelete", addressid, index)
+}
+
+</script>
+
+<style scoped></style>
